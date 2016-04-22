68825dbe8a18f8bfd32bdb06fd0d4ea36a71c1c4
Annemarie Kohler <annemarie.k89@gmail.com>
2016-04-22 11:29:32 +0100

    friday

--- app/js/services/UserService.js
+++ app/js/services/UserService.js
@@ -1,9 +1,6 @@
 promiseApp.service('UserService', ['$http', function($http) {
   this.fetchLoginWithReturn = function(){
     var login;
-
-    // Why is this not best practice when using promises?
-    // Think about why promises were invented in the first place for
     $http.get('http://api.github.com/users').then(function(response){
       $http.get('http://api.github.com/users/' + response.data[0].id).then(function(user){
         login = user.login;
@@ -12,4 +9,19 @@ promiseApp.service('UserService', ['$http', function($http) {
 
     return login;
   };
+  
+  this.fetchLoginWithPromise = function(){
+   return $http.get('http://api.github.com/users')
+     .then(_secondPromise)
+     .then(_handleResponse)
+
+   function _secondPromise(response) {
+     return $http.get('http://api.github.com/users/' + response.data[0].id)
+   }
+
+   function _handleResponse(response){
+     var user = response.data;
+     return user.login;
+   }
+ };
 }]);
